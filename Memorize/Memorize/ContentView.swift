7f2005a493d74742d4ac9330a5de513613bc0d22
Muzammil Peer <muzammilpeer987@gmail.com>
2024-06-04 21:45:11 +0400

    add theme selection

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -8,59 +8,73 @@
 import SwiftUI
 
 struct ContentView: View {
-    let themeColor = Color.orange
-    let emojisList:Array<String> = ["🎃", "👻", "🕸️", "🧛‍♂️", "🧙‍♀️", "😜", "🤡", "😂", "👽", "💀", "😈", "🤖", "👺", "👹", "🍬"]
-    @State var cardCount:Int = 4 // default load 4 items only
+    @State var themeColor = Color.orange
     
+    let emojisList: [String] = ["🎃", "👻", "🕸️", "🧛‍♂️", "🧙‍♀️", "😜", "🤡", "😂", "👽", "💀", "😈", "🤖", "👺", "👹", "🍬"]
+    @State var cardCount: Int = 4 // default load 4 items only
+
     var body: some View {
-        VStack{
-            ScrollView{
-                cardsGrid
+        VStack {
+            ScrollView {
+                self.cardsGrid
             }
             Spacer()
-            bottomButtons
+            self.bottomButtons
         }.padding()
-        
     }
-    
+
     var cardsGrid: some View { LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))], content: {
-        ForEach(0..<cardCount,id:\.self) { index in
-            CardView(isFaceUp: true,content:emojisList[index])
-        }.aspectRatio(2/3,contentMode: .fit)
+        ForEach(0 ..< self.cardCount, id: \.self) { index in
+            CardView(isFaceUp: true, content: self.emojisList[index])
+        }.aspectRatio(2 / 3, contentMode: .fit)
     })
-    .foregroundColor(themeColor)
+    .foregroundColor(self.themeColor)
     .padding()
     }
-    
+
     var bottomButtons: some View {
-        HStack{
-            cardCountAdjusterBy(offset: -1, symbol: "folder.fill.badge.minus")
+        HStack {
+            self.cardCountAdjusterBy(offset: -1, symbol: "folder.fill.badge.minus")
             Spacer()
-            cardCountAdjusterBy(offset: +1, symbol: "folder.fill.badge.plus")
+            themSelector(color: .orange)
+            themSelector(color: .red)
+            themSelector(color: .green)
+            Spacer()
+            self.cardCountAdjusterBy(offset: +1, symbol: "folder.fill.badge.plus")
+            
         }
     }
-    
-    func cardCountAdjusterBy(offset:Int, symbol:String) -> some View {
+
+    func themSelector(color: Color) -> some View {
         Button(action: {
-            cardCount+=offset
+            themeColor = color
+        }, label: {
+            VStack{
+                Image(systemName: "pencil.circle").tint(color)
+                Text(color.description).tint(color)
+            }
+        }).tint(self.themeColor)
+    }
+    
+    func cardCountAdjusterBy(offset: Int, symbol: String) -> some View {
+        Button(action: {
+            self.cardCount += offset
         }, label: {
             Image(systemName: symbol).font(.largeTitle)
-        }).disabled(cardCount+offset < 1 || cardCount+offset > emojisList.count).tint(themeColor)
+        }).disabled(self.cardCount + offset < 1 || self.cardCount + offset > self.emojisList.count).tint(self.themeColor)
     }
 }
 
-
-
 struct CardView: View {
     @State var isFaceUp: Bool = true
-    let content:String
+    let content: String
     var body: some View {
         ZStack {
             let baseRectangle = RoundedRectangle(cornerRadius: 12)
             Group {
                 baseRectangle.fill(.white)
                 baseRectangle.strokeBorder(lineWidth: 2)
-                Text(content).font(.largeTitle)
+                Text(self.content).font(.largeTitle)
             }.opacity(self.isFaceUp ? 1 : 0)
             baseRectangle.opacity(self.isFaceUp ? 0 : 1)
         }.onTapGesture {
