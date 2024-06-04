3c0b54101a8adfed6e1a1c0f9e8d445d4c2d6e0a
Muzammil Peer <muzammilpeer987@gmail.com>
2024-06-04 21:35:18 +0400

    assignment 1

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -8,29 +8,63 @@
 import SwiftUI
 
 struct ContentView: View {
+    let themeColor = Color.orange
+    let emojisList:Array<String> = ["🎃", "👻", "🕸️", "🧛‍♂️", "🧙‍♀️", "😜", "🤡", "😂", "👽", "💀", "😈", "🤖", "👺", "👹", "🍬"]
+    @State var cardCount:Int = 4 // default load 4 items only
+    
     var body: some View {
-        HStack {
-            CardView(isFaceUp: true)
-            CardView()
-            CardView()
-            CardView()
+        VStack{
+            ScrollView{
+                cardsGrid
+            }
+            Spacer()
+            bottomButtons
+        }.padding()
+        
+    }
+    
+    var cardsGrid: some View { LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))], content: {
+        ForEach(0..<cardCount,id:\.self) { index in
+            CardView(isFaceUp: true,content:emojisList[index])
+        }.aspectRatio(2/3,contentMode: .fit)
+    })
+    .foregroundColor(themeColor)
+    .padding()
+    }
+    
+    var bottomButtons: some View {
+        HStack{
+            cardCountAdjusterBy(offset: -1, symbol: "folder.fill.badge.minus")
+            Spacer()
+            cardCountAdjusterBy(offset: +1, symbol: "folder.fill.badge.plus")
         }
-        .foregroundColor(.orange)
-        .padding()
+    }
+    
+    func cardCountAdjusterBy(offset:Int, symbol:String) -> some View {
+        Button(action: {
+            cardCount+=offset
+        }, label: {
+            Image(systemName: symbol).font(.largeTitle)
+        }).disabled(cardCount+offset < 1 || cardCount+offset > emojisList.count).tint(themeColor)
     }
 }
 
+
+
 struct CardView: View {
-    var isFaceUp: Bool = true
+    @State var isFaceUp: Bool = true
+    let content:String
     var body: some View {
         ZStack {
-            if self.isFaceUp {
-                RoundedRectangle(cornerRadius: 12).foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 12).strokeBorder(lineWidth: /*@START_MENU_TOKEN@*/1.0/*@END_MENU_TOKEN@*/)
-                Text("🤡")
-            } else {
-                RoundedRectangle(cornerRadius: 12)
-            }
+            let baseRectangle = RoundedRectangle(cornerRadius: 12)
+            Group {
+                baseRectangle.fill(.white)
+                baseRectangle.strokeBorder(lineWidth: 2)
+                Text(content).font(.largeTitle)
+            }.opacity(self.isFaceUp ? 1 : 0)
+            baseRectangle.opacity(self.isFaceUp ? 0 : 1)
+        }.onTapGesture {
+            self.isFaceUp = !self.isFaceUp
         }
     }
 }
